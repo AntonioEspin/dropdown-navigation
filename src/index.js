d450b67544ef32649c5714cc601a617bf8ca37fd
AntonioEspin <antoesdev91@gmail.com>
2022-09-06 19:36:00 -0500

    [add] media queries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const burgerIcon = document.querySelector('.header__menu');
 const closeIcon = document.querySelector('.header__iconClose');
 const headerNav = document.querySelector('.header__nav');
+const shadow = document.querySelector('.shadow')
 
 const arrowDownFeatures = document.querySelector('.arrow-down-features');
 const featuresList = document.querySelector('.features-list');
@@ -8,15 +9,19 @@ const featuresList = document.querySelector('.features-list');
 const arrowDownCompany = document.querySelector('.arrow-down-company');
 const companyList = document.querySelector('.company-list');
 
+
 // Evento para llamar a la funcion toggle de BurguerMenu
 burgerIcon.addEventListener('click', ()=>{
   headerNav.classList.toggle('inactive')
+  shadow.classList.toggle('inactive')
+
 })
 // ------------------------------------------------------
 
 // Evento para llamar la funcion toggle de closeIcon
 closeIcon.addEventListener('click', () => {
   headerNav.classList.toggle('inactive')
+  shadow.classList.toggle('inactive')
 })
 // ------------------------------------------------------
 
